3bf6e9840979b87def2c1c70ee45ef0e080525f2
SHERNET\gelinama <gelinama@sheridancollege.ca>
2025-03-05 16:28:29 -0500

    Started making 3d buttons

---
+++ Assets/Scripts/3D Button Testing/TestButton3D.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButton3D : MonoBehaviour
+{
+    // x = unpressed Y, y = pressed Y
+    public Vector2 buttonPressY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
